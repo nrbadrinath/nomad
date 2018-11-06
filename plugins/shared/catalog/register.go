d05d09739f9c7b8eba11fe30ba0b637db27dc86c
Nick Ethier <nethier@hashicorp.com>
2018-11-06 00:39:48 -0500

    docker: started work on porting docker driver to new plugin framework

--- plugins/shared/catalog/register.go
+++ plugins/shared/catalog/register.go
@@ -1,10 +1,14 @@
 package catalog
 
-import "github.com/hashicorp/nomad/drivers/rawexec"
+import (
+	"github.com/hashicorp/nomad/drivers/docker"
+	"github.com/hashicorp/nomad/drivers/rawexec"
+)
 
 // This file is where all builtin plugins should be registered in the catalog.
 // Plugins with build restrictions should be placed in the appropriate
 // register_XXX.go file.
 func init() {
 	RegisterDeferredConfig(rawexec.PluginID, rawexec.PluginConfig, rawexec.PluginLoader)
+	RegisterDeferredConfig(docker.PluginID, docker.PluginConfig, docker.PluginLoader)
 }
